84232f7e349f4ded95c60e0f0bc684e2a28281c7
JD Fitzmartin <jondavidodea@gmail.com>
2023-05-26 15:00:07 -0500

    Added .html, .css, and .js files

---
+++ scripts/JewelryStyles.js
@@ -0,0 +1,28 @@
+import { getStyles } from "./database.js"
+
+const styles = getStyles()
+
+document.addEventListener(
+    "change",
+    (event) => {
+    }
+)
+
+export const JewelryStyles = () => {
+    let html = "<ul>"
+
+    // Use .map() for converting objects to <li> elements
+    const listItems = styles.map(style => {
+        return `<li>
+            <input type="radio" name="size" value="${style.id}" /> ${style.style}
+        </li>`
+    })
+
+
+    // Join all of the strings in the array into a single string
+    html += listItems.join("")
+
+    html += "</ul>"
+    return html
+}
+
